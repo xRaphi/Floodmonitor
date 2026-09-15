9a893071a7e1537515bb7b2ebb4f68904d6192ab
xraphi <rtroppmann@student.tgm.ac.at>
2026-09-15 14:58:53 +0200

    konstruktor, getter, setter von Station.java

--- src/main/java/at/ac/tgm/floodmonitor/model/Station.java
+++ src/main/java/at/ac/tgm/floodmonitor/model/Station.java
@@ -1,4 +1,41 @@
 package at.ac.tgm.floodmonitor.model;
 
 public class Station {
+
+    private Long id;
+    private String name;
+    private String location;
+
+    public Station() {
+    }
+
+    public Station(Long id, String name, String location) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
