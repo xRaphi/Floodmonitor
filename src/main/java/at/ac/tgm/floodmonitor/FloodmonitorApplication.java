a4d1132b1d3123ee5f113a229fd87ffa0173aca9
xraphi <rtroppmann@student.tgm.ac.at>
2026-09-15 14:17:09 +0200

    Files Erstellt

---
+++ src/main/java/at/ac/tgm/floodmonitor/FloodmonitorApplication.java
@@ -0,0 +1,13 @@
+package at.ac.tgm.floodmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloodmonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloodmonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/ac/tgm/floodmonitor/FloodmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.tgm.floodmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloodmonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
